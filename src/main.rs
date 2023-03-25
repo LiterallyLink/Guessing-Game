fdcab47be5b4cb260e45580c878758ef62948771
Meow <zechariahpeoples@gmail.com>
2023-03-24 18:36:29 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the Number!");
-    
+
     let min: u32 = 1;
     let max: u32 = 100;
     let secret_number = rand::thread_rng().gen_range(min..=max);
 
+    let mut total_guesses: u64 = 0;
+
     loop {
-        println!("Please input your guess.");
+        println!("Please input your guess.\n");
 
         let mut guess = String::new();
 
@@ -19,7 +21,10 @@ fn main() {
             .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+            Ok(num) => {
+                total_guesses += 1;
+                num
+            }
             Err(_) => continue,
         };
 
@@ -30,8 +35,10 @@ fn main() {
             Ordering::Greater => println!("Too big! Try a smaller number!"),
             Ordering::Equal => {
                 println!("The number was {secret_number}! You Win!");
+                println!("Total Guesses: {total_guesses}");
                 break;
             }
         }
+        
     }
 }
